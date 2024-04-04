14a90e5496f4c8c2aabacfe637ca8ccf602a902c
babislz <gabriela.laureano@escola.pr.gov.br>
2024-04-04 13:28:39 -0300

    phone changes

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,5 @@
 const sql = require("sequelize")
-const database = new sql("FoodSpaceDB", "root", "AKDAgmr007@", {dialect:"mysql", host:"localhost", port:3306})
-// const database = new sql("FoodSpaceDB", "FoodSpaceAdm", "foodspace2024", {dialect:"mssql", host:"localhost", port:52478})
+const database = new sql("FoodSpaceDB", "FoodSpaceAdm", "foodspace2024", {dialect:"mssql", host:"localhost", port:64741})
 
 database.authenticate().then(() => {
     console.log('Connection has been established successfully.');
