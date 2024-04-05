1db6bdd79a88ab11dadc16cccab270d000cdce98
babislz <gabriela.laureano@escola.pr.gov.br>
2024-04-05 16:55:13 -0300

    created new pages and routed the server to them

---
+++ public/js/cadastro_prod.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const img = document.getElementById("image_input");
+const form = document.getElementById("upoad-product-img")
+img.addEventListener("click", () => {
+    form.click();
+});
+})
